b48506339a76b9ca2285a78b81067235fb622ec0
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2023-04-18 20:28:26 +0200

    [ADD] schemes for watchos

---
+++ Love ferret/ferret love Watch App/ferret_loveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ferret_loveApp.swift
+//  ferret love Watch App
+//
+//  Created by Elouan Da Costa Peixoto on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ferret_love_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
